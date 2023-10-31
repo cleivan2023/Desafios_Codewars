28f37180c82c77cba4ff6c502a3d46924d9e8eb5
Carol Leiva N <128113532+cleivan2023@users.noreply.github.com>
2023-10-31 11:49:57 -0300

    Update BuyCarTest.java

--- src/test/java/Kata1/BuyCarTest.java
+++ src/test/java/Kata1/BuyCarTest.java
@@ -1,2 +1,19 @@
-package Kata1;public class BuyCarTest {
+package Kata1;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import org.junit.jupiter.api.Test;
+
+public class BuyCarTest {
+
+    @Test
+    public void Test1() {
+        int[] r = new int[] { 6, 766 };
+        assertArrayEquals(r, BuyCar.nbMonths(2000, 8000, 1000, 1.5));
+    }
+
+    @Test
+    public void Test2() {
+        int[] r = new int[] { 0, 4000 };
+        assertArrayEquals(r, BuyCar.nbMonths(12000, 8000, 1000, 1.5));
+    }
 }
